ea98f79e3ed9c50e6a46e33818c7d07addd2ea1b
Isaac D <isaac_dolphin@outlook.com>
2026-01-28 15:12:49 +0000

    Implemented repository-file.service

--- apps/api/src/repositories/repositories.module.ts
+++ apps/api/src/repositories/repositories.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RepositoriesService } from './repositories.service';
+import { RepositoryFileService } from './repository-file.service';
 import { SyncRepositoriesService } from './sync-repositories.service';
 import { RepositoriesResolver } from './repositories.resolver';
 
 @Module({
   providers: [
     RepositoriesService,
+    RepositoryFileService,
     SyncRepositoriesService,
     RepositoriesResolver,
   ],
-  exports: [RepositoriesService, SyncRepositoriesService],
+  exports: [RepositoriesService, RepositoryFileService, SyncRepositoriesService],
 })
 export class RepositoriesModule {}
